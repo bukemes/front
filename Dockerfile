ed8a3caebdd1df6fe4e4000430fb5f80e8784e5f
Andrei Lavrenov <andrei@lavrenov.io>
2022-08-24 10:20:18 +0200

    CICD
    Dockerized front

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Run the following command to create a Docker image of this project:
+# docker build ./ -t elfensky/bukemes-front:local
+FROM ubuntu
+
+RUN apt-get update
+
+RUN apt-get install nginx -y
+
+COPY /dist /var/www/html/
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-
+import { splitVendorChunkPlugin } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -11,7 +11,8 @@ export default defineConfig({
             devOptions: {
                 enabled: true
             }
-        })
+        }),
+        splitVendorChunkPlugin()
     ],
     server: {
         port: 9003
@@ -24,6 +25,15 @@ export default defineConfig({
     build: {
         target: 'esnext',
         minify: 'esbuild',
-        sourcemap: true
+        sourcemap: true,
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
     }
 });
